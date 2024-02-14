7b0b75c81050dde8dbed33a741a6ced3480ed241
Pedro Vinicius <vinciidev05@gmail.com>
2024-02-14 01:30:17 -0300

    JS finished

--- script.js
+++ script.js
@@ -1,10 +1,49 @@
-let selectElement = document.getElementById('coinSelectOne');
-let selectedCurrency;
-let amount = parseFloat(document.getElementById('valueOne').value);
+function convertValue() {
+    let amount = parseFloat(document.getElementById('firstCoin').value);
+    let coinOne = document.getElementById('coinSelectOne').value
+    let coinTwo = document.getElementById('coinSelectTwo').value
 
-selectElement.addEventListener('change', function() {
-    selectedCurrency = this.value;
-    console.log('Moeda selecionada:', selectedCurrency);
+    const rates = {
+        'USD': {
+            'USD': 1,
+            'BRL': 5.25,
+            'JPY': 109.63,
+            'GBP': 0.72,
+            'BTC': 0.000026
+        },
+        'BRL': {
+            'USD': 0.19,
+            'BRL': 1,
+            'JPY': 20.81,
+            'GBP': 0.14,
+            'BTC': 0.0000049
+        },
+        'JPY': {
+            'USD': 0.0091,
+            'BRL': 0.048,
+            'JPY': 1,
+            'GBP': 0.0065,
+            'BTC': 0.00000023
+        },
+        'GBP': {
+            'USD': 1.39,
+            'BRL': 7.01,
+            'JPY': 153.32,
+            'GBP': 1,
+            'BTC': 0.000036
+        },
+        'BTC': {
+            'USD': 38378.6,
+            'BRL': 202999.36,
+            'JPY': 4425161.74,
+            'GBP': 27579.66,
+            'BTC': 1
+        }
+    };
 
-})
+    const convertedAmount = amount * rates[coinOne][coinTwo];
+
+    document.getElementById('secondCoin').value = convertedAmount.toFixed(2);
+}
+
 
